223ed6f2e5d83ca90487d92294c2712becedbad7
unknown <jeck99@gmail.com>
2020-08-18 10:26:32 +0300

    added public files, body-parser and broweser-sync

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*** Required External Modules מודלים שנצטרך לייבא*/
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser");
 
 /*** App Variables משתנים*/
 const app = express();
@@ -10,12 +11,15 @@ const port = process.env.port | 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug"); //שימוש במנוע פאג להרצת תצוגה מונעת מידע
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /*** Routes Definitions יצירת הראוטניג*/
 app.get('/',(req,res)=>{
     res.render("index")
 })
-
+app.post('/user',(req,res)=>{
+    res.render("user",{user:{name:req.body.name}})
+})
 /*** Server Activation הרצת השרת*/
 app.listen(port)
