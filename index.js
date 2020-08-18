362a85ea088e594672e2c1876c12fd3354970c0c
unknown <jeck99@gmail.com>
2020-08-18 09:28:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*** Required External Modules מודלים שנצטרך לייבא*/
+
+/*** App Variables משתנים*/
+
+/***  App Configuration וההגדרות APP יצירת ה*/
+
+                 //שימוש במנוע פאג להרצת תצוגה מונעת מידע
+
+/*** Routes Definitions יצירת הראוטניג*/
+
+/*** Server Activation הרצת השרת*/
