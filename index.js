b0166a9548cb4f055dc6ca2a3ef78191bb3a884d
unknown <jeck99@gmail.com>
2020-08-18 10:11:07 +0300

    Added External Modules, app and Route Definition

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 /*** Required External Modules מודלים שנצטרך לייבא*/
+const express = require('express');
+const path = require('path');
 
 /*** App Variables משתנים*/
+const app = express();
+const port = process.env.port | 3000;
 
 /***  App Configuration וההגדרות APP יצירת ה*/
 
-                 //שימוש במנוע פאג להרצת תצוגה מונעת מידע
+//שימוש במנוע פאג להרצת תצוגה מונעת מידע
 
 /*** Routes Definitions יצירת הראוטניג*/
+app.get('/',(req,res)=>{
+    res.send("Hello Express")
+})
 
 /*** Server Activation הרצת השרת*/
+app.listen(port)
