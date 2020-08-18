a3f8dc4accdf7632512d6bc2672cc8ff78fd8271
unknown <jeck99@gmail.com>
2020-08-18 10:18:18 +0300

    Added .pug files and view folder

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const app = express();
 const port = process.env.port | 3000;
 
 /***  App Configuration וההגדרות APP יצירת ה*/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug"); //שימוש במנוע פאג להרצת תצוגה מונעת מידע
+app.use(express.static(path.join(__dirname, "public")));
 
-//שימוש במנוע פאג להרצת תצוגה מונעת מידע
 
 /*** Routes Definitions יצירת הראוטניג*/
 app.get('/',(req,res)=>{
-    res.send("Hello Express")
+    res.render("index")
 })
 
 /*** Server Activation הרצת השרת*/
